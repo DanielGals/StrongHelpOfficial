b7dac4562d84d5039119a567fbb32c7600097b51
DanielGals <janjangallaron@gmail.com>
2025-08-23 04:15:50 +0800

    us-024 Add Approver Application Details MVC components
    Introduces controller, view model, and view for Approver Application Details. Sets up the basic structure for future implementation of approver application details functionality.

---
+++ Controllers/Approver/ApproverApplicationDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StrongHelpOfficial.Controllers.Approver
+{
+    public class ApproverApplicationDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ApproverApplicationDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StrongHelpOfficial.Models
+{
+    public class ApproverApplicationDetailsViewModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
