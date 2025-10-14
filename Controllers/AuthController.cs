b293af9b5ff024f3ddd81d949212b042a0604c8a
Axel Jhon C. Sacdal <axeljhonsacdal@gmail.com>
2025-10-14 19:52:42 +0800

    bruh

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -42,7 +42,7 @@ public class AuthController : Controller
 
         // Bypass AD authentication for testing
         userModel.IsAuthenticated = true; // Simulate authentication
-        userModel.Email = "liezelrobles@Stronghelps.local"; // Hardcoded email for testing
+        userModel.Email = "jaspermanalo@StrongHelps.local"; // Hardcoded email for testing
         userModel.Username = "asdasd";
         userModel.Domain = "Sample Tae";
 
